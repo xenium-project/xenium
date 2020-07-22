def6c56c6918d93134c1640741838f50cc6ec9b9
TheDevMinerTV <tobigames200@gmail.com>
2020-07-22 20:09:25 +0200

    Update supported version

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -187,7 +187,7 @@ namespace CryptoNote
         };
 
         /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
-        const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX     =                                                           1;
+        const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX     =                                                           2;
         const uint64_t FORK_HEIGHTS_SIZE                 =                sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
 
         /* The index in the FORK_HEIGHTS array that this version of the software will
@@ -248,11 +248,11 @@ namespace CryptoNote
 
     // P2P Network Configuration Section - This defines our current P2P network version
     // and the minimum version for communication between nodes
-    const uint8_t  P2P_CURRENT_VERSION                       =                1;
+    const uint8_t  P2P_CURRENT_VERSION                       =                2;
     const uint8_t  P2P_MINIMUM_VERSION                       =                1;
 
     // This defines the minimum P2P version required for lite blocks propogation
-    const uint8_t  P2P_LITE_BLOCKS_PROPOGATION_VERSION       =                1;
+    const uint8_t  P2P_LITE_BLOCKS_PROPOGATION_VERSION       =                2;
 
     // This defines the number of versions ahead we must see peers before we start displaying
     // warning messages that we need to upgrade our software.
